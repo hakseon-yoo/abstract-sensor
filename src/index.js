9b27d3400e046025a97fd8d6b99ba59b901b391d
hakseon-yoo <gkrtjs2020@naver.com>
2022-04-04 09:51:06 +0900

    2022. 04. 04. hsyoo 매운맛 과제 끝

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
-class Sensor {}
+class Sensor {
+    constructor(){
+        this.powerStatus = 'off';
+        this.status = 'idle';
+        this.reportingInterval = 10000
+    }
+    turn(power){
+        if(power === 'off') this.powerStatus = 'off';
+        if(this.powerStatus === 'on') throw Error();
 
-class IotServer {}
+        this.powerStatus = power;
+
+        setTimeout(() => { this.status = 'sensingDistance'; }, this.reportingInterval);
+        setTimeout(() => { this.status = 'reportingData'; }, this.reportingInterval + 500);
+        setTimeout(() => { this.status = 'idle'; }, this.reportingInterval + 500 + 1000);
+
+        console.log(this.reportingInterval);
+    }
+}
+
+class IotServer {
+    constructor(){
+        this.Sensor;
+    }
+    
+    start(arrSensor){
+        this.Sensor = arrSensor[0];
+    }
+    
+    publish(obj){
+        if(this.Sensor.powerStatus === 'off') return;
+        if(obj.actionId === 'CHANGE_REPORTING_INTERVAL') this.Sensor.reportingInterval = obj.payload;
+    }
+}
 
 module.exports = {
     Sensor,
